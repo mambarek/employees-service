f5f5046ce93c6b692a1a532ded088a845b26e9a8
Mohamed Ali Mbarek <mbarek@it-2go.de>
2020-12-30 23:16:40 +0100

    save new projects and update old one via jms

--- src/main/java/com/it2go/micro/employeesservice/mapper/ProjectMapper.java
+++ src/main/java/com/it2go/micro/employeesservice/mapper/ProjectMapper.java
@@ -3,6 +3,7 @@ package com.it2go.micro.employeesservice.mapper;
 import com.it2go.micro.employeesservice.persistence.jpa.entities.ProjectEntity;
 import com.it2go.micro.projectmanagement.domain.Project;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 /**
  * created by mmbarek on 28.12.2020.
@@ -13,4 +14,6 @@ public interface ProjectMapper {
   Project projectEntityToProject(ProjectEntity projectEntity);
 
   ProjectEntity projectToProjectEntity(Project project);
+  ProjectEntity updateProject(@MappingTarget ProjectEntity projectEntity, Project project);
+
 }
--- src/main/java/com/it2go/micro/employeesservice/services/ProjectService.java
+++ src/main/java/com/it2go/micro/employeesservice/services/ProjectService.java
@@ -2,12 +2,15 @@ package com.it2go.micro.employeesservice.services;
 
 import com.it2go.micro.projectmanagement.domain.Project;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * created by mmbarek on 30.12.2020.
  */
 public interface ProjectService {
 
+  Project findByPublicId(String publicId);
+  Project findByPublicId(UUID publicId);
   Project saveProject(Project project);
   Project updateProject(Project project);
   List<Project> findAll();
